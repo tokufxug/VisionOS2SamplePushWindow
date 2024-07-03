4798a79e0c3256292c1410d2bfa53e9621475c11
tokufxug <tokufxug@gmail.com>
2024-07-03 22:16:38 +0900

    Initial Commit

---
+++ VisionOS2SamplePushWindow/VisionOS2SamplePushWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS2SamplePushWindowApp.swift
+//  VisionOS2SamplePushWindow
+//
+//  Created by Sadao Tokuyama on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS2SamplePushWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOS2SamplePushWindowTests/VisionOS2SamplePushWindowTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS2SamplePushWindowTests.swift
+//  VisionOS2SamplePushWindowTests
+//
+//  Created by Sadao Tokuyama on 7/3/24.
+//
+
+import Testing
+@testable import VisionOS2SamplePushWindow
+
+struct VisionOS2SamplePushWindowTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
